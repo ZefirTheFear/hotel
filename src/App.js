df76676eaf5f5a8a51d97eaf2354c6a5e14fcb8e
ZefirTheFear <z4clr07.gaming@gmail.com>
2020-01-12 21:20:28 +0200

    ready for deploy

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Rooms from "./pages/Rooms/Rooms";
 import SingleRoom from "./pages/SingleRoom/SingleRoom";
 import NotFound from "./pages/NotFound/NotFound";
 
-// import "./App.css";
-
 function App() {
   return (
     <>
